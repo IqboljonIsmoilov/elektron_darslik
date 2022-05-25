3011687ac311e56ff2eb585afe09fb7642e876ad
IQBOLJON <iqboljon.ismoilov.98mail.ru>
2022-05-25 18:49:40 +0500

    diplom_ishim

---
+++ JS/JavaScript.js
@@ -0,0 +1,22 @@
+
+// let icon = document.getElementById('icon')
+
+// icon.onclick = function() {
+//     document.body.classList.toggle('dark-theme')
+//     if(document.body.classList.contains('dark-theme')) {
+//     icon.src = '/Diplom/img/sun.png'
+// } else {
+//     icon.src = '/Diplom/img/moon.png'
+// }
+// }
+
+// let visible = true ;
+//           function showFun() {
+//             if(visible) {
+//               document.getElementById('myShowBlock' ).style.display = 'none';
+//               visible = false;
+//             } else {
+//               document.getElementById('myShowBlock' ).style.display = 'block';
+//               visible = true;
+//             }
+//       }
